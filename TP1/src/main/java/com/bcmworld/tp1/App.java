9816fc16a3d0757911542dcbe96755f1e7dad19d
SantiagoMinian <minian.santiago@gmail.com>
2017-03-20 08:34:26 -0300

    Added spark hello world

--- TP1/src/main/java/com/bcmworld/tp1/App.java
+++ TP1/src/main/java/com/bcmworld/tp1/App.java
@@ -1,13 +1,10 @@
 package com.bcmworld.tp1;
 
-/**
- * Hello world!
- *
- */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+import static spark.Spark.*;
+
+public class App {
+    public static void main(String[] args) {
+        get("/hello", (req, res) -> "Hello Rochi");
+        get("/saclier", (req, res) -> "Lucas");
     }
 }
